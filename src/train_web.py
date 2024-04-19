8f1aa670c0aa93f2dab59e8b60f02486e66d3333
jungskky <38771771+jungskky@users.noreply.github.com>
2024-04-19 22:00:24 +0900

    Update train_web.py

--- src/train_web.py
+++ src/train_web.py
@@ -2,7 +2,7 @@ from llmtuner import create_ui
 
 
 def main():
-    create_ui().queue().launch(server_name="0.0.0.0", server_port=None, share=False, inbrowser=True)
+    create_ui().queue().launch(server_name="0.0.0.0", server_port=None, share=True, inbrowser=True)
 
 
 if __name__ == "__main__":
